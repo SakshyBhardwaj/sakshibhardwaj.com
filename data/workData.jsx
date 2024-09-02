f156f8fb9e94c53bdaa2d8e8276a3ecab678ddc5
SakshyBhardwaj <sakshi.s1592@gmail.com>
2024-09-02 14:29:52 +0530

    selfimages

--- data/workData.jsx
+++ data/workData.jsx
@@ -9,19 +9,19 @@ export const workData = [
     },
     {
         nos: `02`,
-        title: `Computational Art Algorithms`,
-        tags: `Creative Coding`,
-        desc: `Developing computational art algorithms for dice art and string art`,
+        title: `Ryanair Case Study`,
+        tags: `Case Study`,
+        desc: `Ryanair UX Case study`,
         imgsrc: `images/work/iitgn.jpg`,
-        link: `https://medium.com/@747.aamir/computational-art-algorithms-6f4686115ddd`
+        link: `https://medium.com/@sakshy/case-study-identifying-and-resolving-bugs-in-ryanair-9f531435e2e3`
     },
     {
         nos: `03`,
-        title: `Group Ordering in Swiggy`,
-        tags: `Product Design`,
-        desc: `Designing a feature for Swiggy (a food delivery app) that allows a group of people to order food together`,
+        title: `Infographics`,
+        tags: `Infographic Design`,
+        desc: `Designing a Infographic for Infographic for Anti-Counterfeit Packaging Market`,
         imgsrc: `images/work/swiggy3.png`,
-        link: `https://medium.com/@747.aamir/designing-a-group-ordering-experience-in-swiggy-dd26b7942177`
+        link: `https://medium.com/design-bootcamp/a-ux-approach-to-infographic-for-anti-counterfeit-packaging-market-454fcef757e7`
     },
     {
         nos: `04`,
